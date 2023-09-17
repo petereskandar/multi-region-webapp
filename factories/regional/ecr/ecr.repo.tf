11f6911c19ddeab3fb9851f9563e0dad1e8a8479
Peter Eskandar <peter.eskandar@posteitaliane.it>
2023-09-17 19:43:51 +0200

    update ECR Login

--- factories/regional/ecr/ecr.repo.tf
+++ factories/regional/ecr/ecr.repo.tf
@@ -31,44 +31,31 @@ module "ecr" {
   tags = var.tags
 }
 
+# Equivalent of aws ecr get-login
+data "aws_ecr_authorization_token" "ecr_token" {}
+
 // Login to ECR Repo
 // Build the sample WebApp
 // tag and push to the repo
-resource "null_resource" "ecr_login" {
+resource "null_resource" "docker_packaging" {
+  # aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com 
+  # echo ${data.aws_ecr_authorization_token.ecr_token.password} | docker login —username ${data.aws_ecr_authorization_token.ecr_token.user_name} —password-stdin ${data.aws_ecr_authorization_token.ecr_token.proxy_endpoint}
+  # docker login --username AWS -p $(aws ecr get-login-password --region ${data.aws_region.current.name} ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com
   provisioner "local-exec" {
-    interpreter = ["cmd", "-Command"]
-    command     = "aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin"
+    interpreter = ["PowerShell", "-Command"]
+    command     = <<EOF
+        docker login --username AWS -p $(aws ecr get-login-password --region ${data.aws_region.current.name} ) ${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com      
+        docker build --build-arg REGION=${data.aws_region.current.name} -t web-app-repo-${data.aws_region.current.name} ${path.module}/webapp 
+        docker tag web-app-repo-${data.aws_region.current.name}:latest ${module.ecr.repository_url}:latest 
+	      docker push ${module.ecr.repository_url}:latest
+	    EOF
   }
 
   triggers = {
-    "run_at" = timestamp()
+    token_expired = data.aws_ecr_authorization_token.ecr_token.expires_at
   }
 
   depends_on = [
     module.ecr
   ]
 }
-
-// Login to ECR Repo
-// Build the sample WebApp
-// tag and push to the repo
-resource "null_resource" "docker_packaging" {
-  ## aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com 
-  provisioner "local-exec" {
-    interpreter = ["PowerShell", "-Command"]
-    command     = <<-EOF
-        docker build --build-arg REGION=${data.aws_region.current.name} -t web-app-repo ${path.module}/webapp 
-        docker tag web-app-repo:latest ${module.ecr.repository_url}:latest 
-	      docker push ${module.ecr.repository_url}:latest
-	    EOF
-  }
-
-  triggers = {
-    "run_at" = timestamp()
-  }
-
-  depends_on = [
-    null_resource.ecr_login
-  ]
-}
-
