53f88a533995d1749fa936179076715cb0024218
Peter Eskandar <peter.eskandar@posteitaliane.it>
2023-09-17 17:32:55 +0200

    readme

--- factories/regional/ecr/ecr.repo.tf
+++ factories/regional/ecr/ecr.repo.tf
@@ -34,11 +34,29 @@ module "ecr" {
 // Login to ECR Repo
 // Build the sample WebApp
 // tag and push to the repo
+resource "null_resource" "ecr_login" {
+  provisioner "local-exec" {
+    interpreter = ["cmd", "-Command"]
+    command     = "aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin"
+  }
+
+  triggers = {
+    "run_at" = timestamp()
+  }
+
+  depends_on = [
+    module.ecr
+  ]
+}
+
+// Login to ECR Repo
+// Build the sample WebApp
+// tag and push to the repo
 resource "null_resource" "docker_packaging" {
+  ## aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com 
   provisioner "local-exec" {
     interpreter = ["PowerShell", "-Command"]
-    command     = <<EOF
-        aws ecr get-login-password --region ${data.aws_region.current.name} | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.${data.aws_region.current.name}.amazonaws.com 
+    command     = <<-EOF
         docker build --build-arg REGION=${data.aws_region.current.name} -t web-app-repo ${path.module}/webapp 
         docker tag web-app-repo:latest ${module.ecr.repository_url}:latest 
 	      docker push ${module.ecr.repository_url}:latest
@@ -50,7 +68,7 @@ resource "null_resource" "docker_packaging" {
   }
 
   depends_on = [
-    module.ecr
+    null_resource.ecr_login
   ]
 }
 
